490eef8ad753a3c9d3dd42acc8cb4470bccba327
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2020-07-06 08:55:22 +0700

    Update BloggerApi.kt

--- MyBlog/app/src/main/java/com/azhar/myblog/networking/BloggerApi.kt
+++ MyBlog/app/src/main/java/com/azhar/myblog/networking/BloggerApi.kt
@@ -7,15 +7,15 @@ package com.azhar.myblog.networking
 object BloggerApi {
 
     var BASE_URL = "https://www.googleapis.com/blogger/v3/blogs/"
-    var API_KEY = "AIzaSyBP7_LCltLov1TZNwBGwgcFqpYjw2IzQCE"
-    var BLOGGER_ID = "ID BLOG"
+    var API_KEY = "API_KEY"
+    var BLOGGER_ID = "ID_BLOG"
 
     var ListPost = "$BASE_URL$BLOGGER_ID/posts?key=$API_KEY"
 
-    var About = "$BASE_URL$BLOGGER_ID/pages/ID PAGES?key=$API_KEY"
+    var About = "$BASE_URL$BLOGGER_ID/pages/ID_PAGES?key=$API_KEY"
 
-    var PrivacyPolicy = "$BASE_URL$BLOGGER_ID/pages/ID PAGE?key=$API_KEY"
+    var PrivacyPolicy = "$BASE_URL$BLOGGER_ID/pages/ID_PAGE?key=$API_KEY"
 
-    var Disclaimer = "$BASE_URL$BLOGGER_ID/pages/ID PAGE?key=$API_KEY"
+    var Disclaimer = "$BASE_URL$BLOGGER_ID/pages/ID_PAGE?key=$API_KEY"
 
 }
